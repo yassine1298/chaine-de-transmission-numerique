6a1f791f5a31752174f80b3ae141354e459b9b22
yassin chetouani <yassine07ch@gmail.com>
2021-10-09 10:40:34 +0000

    Upload New File

---
+++ Codes_sources/rz.m
@@ -0,0 +1,20 @@
+function [t,x] = rz(bits, e)
+
+T = length(bits)/e; % longeur de tout la sequence de bits
+                          % e =Nbr de bits par seconds
+                          % bits = sequence de bits 
+n = 1000;
+N = n*length(bits);
+dt = T/N;
+t = 0:dt:T;
+x = zeros(1,length(t)); % signal de sortie
+
+for i = 0:length(bits)-1
+  if bits(i+1) == 1
+    x(i*n+1:(i+0.5)*n) = 1;
+ 
+  else
+    x(i*n+1:(i+1)*n) = 0;
+  end
+end
+
